01584b56d5c5c66485ccfc0fc63657e121e56f10
Anshulight <mrray7838035317@gmail.com>
2020-05-30 15:14:00 +0530

    ui ready

--- app/src/main/java/com/example/git1pro/QuesBank.java
+++ app/src/main/java/com/example/git1pro/QuesBank.java
@@ -1,4 +1,28 @@
 package com.example.git1pro;
 
 public class QuesBank {
+    private  String ques;
+    private  boolean ans;
+
+    public QuesBank(String ques, boolean ans) {
+        this.ques = ques;
+        this.ans = ans;
+    }
+
+    public void setQues(String ques) {
+        this.ques = ques;
+    }
+
+    public void setAns(boolean ans) {
+        this.ans = ans;
+    }
+
+    public String getQues() {
+        return ques;
+    }
+
+    public boolean isAns() {
+        return ans;
+    }
+
 }
